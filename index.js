a16fb863002b8b2607f179b2784c3593bb1e4c4b
ryuken73 <ryuken73@gmail.com>
2022-03-13 18:39:38 +0900

    install: googleapis

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const restify = require('restify');
 const getVideoInfo = require('./routes/getVideoInfo');
+const searchLiveVideos = require('./routes/searchLiveVideos');
+const subscribeCallback = require('./routes/subscribeCallback');
 
 const server = restify.createServer();
 server.use(restify.plugins.queryParser());
-server.get('/ytinfo/:videoId', getVideoInfo);
+server.get('/ytinfo/getHlsUrl/:videoId', getVideoInfo);
+server.get('/ytinfo/searchLiveVideos/:channelId', searchLiveVideos);
+server.get('/youtube/subscribe/callback', subscribeCallback);
 
 server.listen(8000, () => console.log(`listening ${server.name}: ${server.url}`))
