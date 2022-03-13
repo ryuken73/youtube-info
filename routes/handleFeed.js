fe13aa135561e5d24d4ca313619b38f6a5728a83
ryuken73 <ryuken73@gmail.com>
2022-03-13 20:56:03 +0900

    fix: pubsub callback result shoud not be Buffer

--- routes/handleFeed.js
+++ routes/handleFeed.js
@@ -1,6 +1,9 @@
 const handleFeed = (req, res, next) => {
     const body = req.body;
+    const contentType = req.header('Content-Type');
+    console.log(contentType)
     console.log(body)
+    console.log(body.toString())
     res.send(204);
 }
 
