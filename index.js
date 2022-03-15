56229a537f7819cefb6236b7a33dcd231b7afd61
ryuken73 <ryuken73@gmail.com>
2022-03-15 23:05:05 +0900

    update: complete SSE server!

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const getVideoInfo = require('./routes/getVideoInfo');
 const searchLiveVideos = require('./routes/searchLiveVideos');
 const subscribeCallback = require('./routes/subscribeCallback');
 const handleFeed = require('./routes/handleFeed');
-const createSSEServer = require('./lib/sseClass');
-const useSSE = createSSEServer();
+const {useWaitEvent, useBroadcast} = require('./lib/sseClass');
 
 const server = restify.createServer();
 server.use(restify.plugins.queryParser());
@@ -13,27 +12,14 @@ server.pre(restify.plugins.pre.context());
 server.get('/ytinfo/getHlsUrl/:videoId', getVideoInfo);
 server.get('/ytinfo/searchLiveVideos/:channelId', searchLiveVideos);
 server.get('/youtube/subscribe/callback', subscribeCallback);
-server.post('/youtube/subscribe/callback', useSSE, handleFeed);
-server.get('/pushMessage', useSSE, (req, res) => {
-    const {event='ALL', message="hello"} = req.query;
-    const sse = req.get('sse');
-    sse.broadcast(event, message)
-    res.send('ok');
+server.post('/youtube/subscribe/callback', useBroadcast, handleFeed);
+server.get('/pushMessage', useBroadcast, (req, res) => {
+    const {event, message="hello"} = req.query;
+    const sendCount = req.broadcast(event, message);
+    res.send({success: true, result: sendCount});
 })
-server.get('/waitEvent', useSSE, (req, res) => {
-    const {event='ALL'} = req.query
-    const sse = req.get('sse');
-
-    const headers = {
-        'Content-Type': 'text/event-stream',
-        'Connection': 'keep-alive',
-        'Cache-Control': 'no-cache'
-    };
-    res.set(headers);
-    res.write('wait event...\n\n');
-    req.on('close', () => {
-        sse.unWaitEvent(event, res)
-    })
-    sse.waitEvent(event, res);
+server.get('/waitEvent', useWaitEvent, (req, res) => {
+    const {event} = req.query;
+    req.waitEvent(event, res)
 })
 server.listen(8000, () => console.log(`listening ${server.name}: ${server.url}`))
