6a7aff8550291f7481ac93e4df5696f789dc0715
ryuken73 <ryuken73@gmail.com>
2022-03-16 00:03:59 +0900

    making: parse rss feed of youtube video

--- read_atom.js
+++ read_atom.js
@@ -1,7 +1,9 @@
 const feed = require('feed-read-parser');
+const convert = require('xml-js');
 const atomString = `
 <?xml version='1.0' encoding='UTF-8'?>
-<feed xmlns:yt="http://www.youtube.com/xml/schemas/2015" xmlns="http://www.w3.org/2005/Atom"><link rel="hub" href="https://pubsubhubbub.appspot.com"/><link rel="self" href="https://www.youtube.com/xml/feeds/videos.xml?channel_id=UChlgI3UHCOnwUGzWzbJ3H5w"/><title>YouTube video feed</title><updated>2022-03-15T14:31:08.139437215+00:00</updated><entry>
+<feed xmlns:yt="http://www.youtube.com/xml/schemas/2015" xmlns="http://www.w3.org/2005/Atom"><link rel="hub" href="https://pubsubhubbub.appspot.com"/><link rel="self" href="https://www.youtube.com/xml/feeds/videos.xml?channel_id=UChlgI3UHCOnwUGzWzbJ3H5w"/><title>YouTube video feed</title><updated>2022-03-15T14:31:08.139437215+00:00</updated>
+ <entry>
   <id>yt:video:LYroskqGn3M</id>
   <yt:videoId>LYroskqGn3M</yt:videoId>
   <yt:channelId>UChlgI3UHCOnwUGzWzbJ3H5w</yt:channelId>
@@ -13,7 +15,21 @@ const atomString = `
   </author>
   <published>2022-03-15T14:28:46+00:00</published>
   <updated>2022-03-15T14:31:08.139437215+00:00</updated>
- </entry></feed>
+ </entry>
+ <entry>
+  <id>yt:video:LYroskqGn3M</id>
+  <yt:videoId>LYroskqGn3M</yt:videoId>
+  <yt:channelId>UChlgI3UHCOnwUGzWzbJ3H5w</yt:channelId>
+  <title>YTN</title>
+  <link rel="alternate" href="https://www.youtube.com/watch?v=LYroskqGn3M"/>
+  <author>
+   <name>YTN news</name>
+   <uri>https://www.youtube.com/channel/UChlgI3UHCOnwUGzWzbJ3H5w</uri>
+  </author>
+  <published>2022-03-15T14:28:46+00:00</published>
+  <updated>2022-03-15T14:31:08.139437215+00:00</updated>
+ </entry> 
+ </feed>
 `
 feed.atom(atomString, (err, articles) => {
     if(err){
@@ -22,3 +38,10 @@ feed.atom(atomString, (err, articles) => {
     }
     console.log(articles)
 })
+
+const xml = JSON.parse(convert.xml2json(atomString, {compact:true}));
+const ytId = xml['yt:videoId'];
+console.log(xml)
+console.log(xml['feed']['entry'])
+console.log(xml['feed']['entry']['yt:videoId']['_text'])
+console.log(ytId)
