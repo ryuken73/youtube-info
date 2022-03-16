5acb1a738f3df61a5d97e6d69014eea87bdae3b9
ryuken73 <ryuken73@gmail.com>
2022-03-16 18:37:00 +0900

    update: send Server Send Event when feed arrived

--- routes/handleFeed.js
+++ routes/handleFeed.js
@@ -1,13 +1,24 @@
 const parseFeed = require('../lib/parseYoutubeAtom');
+const CONSTANTS = require('../config.json');
+
+const {EVENTS} = CONSTANTS;
+
+const getEventName = channelId => {
+    const channel = EVENTS.find(event => event.id === channelId)
+    return channel.eventName || 'NOT-DEFINED';
+}
 
 const handleFeed = async (req, res, next) => {
     const body = req.body;
-    // const contentType = req.header('Content-Type');
-    // console.log(contentType)
-    // console.log(body)
     const stringifiedXML = body.toString();
     const parsed = await parseFeed(stringifiedXML);
+    const {channelId} = parsed;
+    // send SSE
+    const eventName = getEventName(channelId);
+    const sendCount = req.broadcast(eventName, parsed);
+    //
     console.log(parsed);
+    console.log(sendCount);
     res.send(204);
 }
 
