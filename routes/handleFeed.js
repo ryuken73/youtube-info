3cb64c3171c886b05136e90574be9ce5ce5528f0
ryuken73 <ryuken73@gmail.com>
2022-03-16 21:15:12 +0900

    fix: get eventName from channelId

--- routes/handleFeed.js
+++ routes/handleFeed.js
@@ -6,7 +6,6 @@ console.log(CONSTANTS, EVENTS);
 
 const getEventName = channelId => {
     const channel = EVENTS.find(event => event.id === channelId)
-    console.log(channelId, EVENTS, channel)
     const {eventName} = channel 
     return eventName;
 }
@@ -14,17 +13,19 @@ const getEventName = channelId => {
 const handleFeed = async (req, res, next) => {
     const body = req.body;
     const stringifiedXML = body.toString();
-    const parsed = await parseFeed(stringifiedXML);
-    const {channelId} = parsed;
-    // send SSE
-    const eventName = getEventName(channelId);
-    console.log(`eventName[${eventName}, channelId[${channelId}]]`);
-    const parsedWithEventName = {...parsed, eventName};
-    const sendCount = req.broadcast(eventName, JSON.stringify(parsedWithEventName));
-    //
-    console.log(parsed);
-    console.log(sendCount);
-    res.send(204);
+    // const parsed = await parseFeed(stringifiedXML);
+    const parsedEntries = await parseFeed(stringifiedXML);
+    parsedEntries.forEach(parsed => {
+        const {channelId} = parsed;
+        // send SSE
+        const eventName = getEventName(channelId);
+        console.log(`eventName[${eventName}, channelId[${channelId}]]`);
+        const parsedWithEventName = {...parsed, eventName};
+        const sendCount = req.broadcast(eventName, JSON.stringify(parsedWithEventName));
+        console.log(parsed);
+        console.log(sendCount);
+        res.send(204);
+    })
 }
 
 module.exports = handleFeed;
